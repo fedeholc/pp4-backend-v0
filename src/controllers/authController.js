618e9a6a4546131ef9646e7277692de79ef82bb6
Federico Holc <federicoholc@gmail.com>
2025-05-17 15:49:27 -0300

    Add services for retrieving client and technician details in login function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,6 @@
 import * as authService from "../services/authService.js";
+import * as clienteService from "../services/clienteService.js";
+import * as tecnicoService from "../services/tecnicoService.js";
 import jwt from "jsonwebtoken";
 import process from "node:process";
 const allowedRoles = ["cliente", "tecnico", "admin"];
@@ -54,6 +56,23 @@ export async function login(req, res, next) {
         .status(400)
         .json({ message: "La contraseña debe tener al menos 4 caracteres" });
     const result = await authService.login({ email, password });
+    if (!result) {
+      return res.status(401).json({ message: "Credenciales inválidas" });
+    }
+    // Generar token JWT
+    console.log("result", result);
+    if (result.user.rol === "cliente") {
+      let response = await clienteService.getClienteByUserId(result.user.id);
+      console.log("response cliente", response);
+      if (response) {
+        result.user.cliente = response;
+      }
+    } else if (result.user.rol === "tecnico") {
+      let response = await tecnicoService.getTecnicoByUserId(result.user.id);
+      if (response) {
+        result.user.tecnico = response;
+      }
+    }
     res.json(result);
   } catch (err) {
     next(err);
