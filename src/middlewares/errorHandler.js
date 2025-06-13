756dbbb2b6bac49bf772d32c1363a2f011003537
ML_DEV <martinlopez@bc.org.ar>
2025-06-13 18:58:53 -0300

    "errorHandler"

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,12 @@
 /**
  * Middleware para manejo de errores global.
  */
-export function errorHandler(err, req, res) {
-  console.error(err);
+
+/**
+ * Middleware para manejo de errores global.
+ */
+export function errorHandler(err, req, res, next) {
+  console.error(err); // Log completo del error
   res.status(500).json({ message: "Error interno del servidor" });
 }
+
